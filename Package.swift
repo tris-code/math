942aba84d627140124dd95d3e9f7d87fa6551156
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-21 01:42:08 +0000

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Math",
+    products: [
+        .library(
+            name: "Math",
+            targets: ["Math"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/tris-foundation/test.git",
+            .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "Math",
+            dependencies: []),
+        .testTarget(
+            name: "MathTests",
+            dependencies: ["Test", "Math"])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,7 @@
+# Math
+
+## Package.swift
+
+```swift
+.package(url: "https://github.com/tris-foundation/math.git", .branch("master"))
+```
